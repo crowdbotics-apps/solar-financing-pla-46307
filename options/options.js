860fbed651606d6049fb181d4961e16b2f03ddfb
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:43:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-financing-pla-46307",
+  url: "https://solar-financing-pla-46307.botics.co",
+  api: "https://solar-financing-pla-46307.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
